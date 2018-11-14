e5838cd5789266c999856f7aa68fc1bd42f23931
jorgediegocrespo <jorgediegocrespo@gmail.com>
2018-11-14 07:24:54 +0100

    Changes to manage push notifications in android

--- PushNotifications/PushNotifications.Android/MainActivity.cs
+++ PushNotifications/PushNotifications.Android/MainActivity.cs
@@ -1,15 +1,16 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter.Push;
 
 namespace PushNotifications.Droid
 {
-    [Activity(Label = "PushNotifications", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "PushNotifications", 
+              Icon = "@mipmap/icon", 
+              Theme = "@style/MainTheme", 
+              MainLauncher = true, 
+              LaunchMode = LaunchMode.SingleTop,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -20,6 +21,12 @@ namespace PushNotifications.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        protected override void OnNewIntent(Android.Content.Intent intent)
+        {
+            base.OnNewIntent(intent);
+            Push.CheckLaunchedFromNotification(this, intent);
         }
     }
 }
--- PushNotifications/PushNotifications/App.xaml.cs
+++ PushNotifications/PushNotifications/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,13 +12,41 @@ namespace PushNotifications
         public App()
         {
             InitializeComponent();
-
             MainPage = new MainPage();
         }
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            if (!AppCenter.Configured)
+                Push.PushNotificationReceived += Push_PushNotificationReceived;
+
+            AppCenter.Start("ios=4c8acd8f-9c6f-4769-9a80-83aa120e146a;" +
+                            "android=19cf5020-4f59-4128-89bb-ec3e1cc6ae04;",
+            typeof(Push));
+            //AppCenter.Start("19cf5020-4f59-4128-89bb-ec3e1cc6ae04", typeof(Push));
+
+        }
+
+        private void Push_PushNotificationReceived(object sender, PushNotificationReceivedEventArgs e)
+        {
+            // Add the notification message and title to the message
+            var summary = $"Push notification received:" +
+                                $"\n\tNotification title: {e.Title}" +
+                                $"\n\tMessage: {e.Message}";
+
+            // If there is custom data associated with the notification,
+            // print the entries
+            if (e.CustomData != null)
+            {
+                summary += "\n\tCustom data:\n";
+                foreach (var key in e.CustomData.Keys)
+                {
+                    summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                }
+            }
+
+            // Send the notification summary to debug output
+            System.Diagnostics.Debug.WriteLine(summary);
         }
 
         protected override void OnSleep()
